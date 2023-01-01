7577bfda098b8dccceeefa8f9ea5e8935e23081c
jayantwaldia <imjayantwaldia@gmail.com>
2023-01-01 18:04:21 +0530

    rq-features

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -7,16 +7,29 @@ const fetchSuperHeroes = () => {
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, isError, error, data }: any = useQuery(
+  const onSuccess = (data: any) => console.log('Data fetching completed', data);
+  const onError = (error: any) => console.log('Data fetching failed...', error);
+  const { isLoading, isError, error, data, refetch }: any = useQuery(
     'super-heroes',
-    fetchSuperHeroes
+    fetchSuperHeroes,
+    {
+      refetchOnMount: true, // when a component mounts
+      refetchOnWindowFocus: 'always', // when tab switches
+      enabled: false, // disables auto fetch when component mounts
+      onSuccess,
+      onError,
+      // refetchInterval: 2000, fetching interval
+      // refetchIntervalInBackground: true,
+      // cacheTime: 5000, remains in cache for set time
+    }
   );
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>{error.message}</p>;
   return (
     <>
       <h2>React Query fetched Super Heroes Page</h2>
-      {data.map((hero: any) => {
+      <button onClick={refetch}>Fetch Heroes</button>
+      {data?.map((hero: any) => {
         return <div key={hero.id}>{hero.name}</div>;
       })}
     </>
